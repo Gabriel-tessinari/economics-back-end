f539aa4cf8549164cd03523bded9a9e2a8a29ae3
Gabriel Tessinari <gtcmiranda18@gmail.com>
2022-09-30 10:27:35 -0300

    Adding swagger docs with get all accounts route documentation

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
 
 const options: cors.CorsOptions = {
   allowedHeaders: [
@@ -23,6 +25,7 @@ const app = express();
 
 app.use(cors(options));
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(routes);
 
 app.listen(PORT, () => console.log('Servidor iniciado na porta ' + PORT));
